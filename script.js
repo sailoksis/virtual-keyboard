593f9139cfbe12906d36b6e78c7fee75d820c472
Aleksandr <shunko1999@gmail.com>
2023-04-25 23:58:29 +0300

    fix: add const for tegs

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-let containerDiv = document.createElement('div');
+const containerDiv = document.createElement('div');
 containerDiv.classList.add('container');
 
-let textareaDiv = document.createElement('textarea');
+const textareaDiv = document.createElement('textarea');
 textareaDiv.classList.add('main__textarea');
 
-let keyboardDiv = document.createElement('div');
+const keyboardDiv = document.createElement('div');
 keyboardDiv.id = 'keyboard';
 
 containerDiv.appendChild(textareaDiv);
 containerDiv.appendChild(keyboardDiv); // Добавляем keyboardDiv как дочерний элемент containerDiv
-document.body.appendChild(containerDiv);
+document.body.appendChild(containerDiv
+    );
 textareaDiv.value = 'Input some text';
